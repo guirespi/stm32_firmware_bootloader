8fc67580ae4edbc3a3f872b65da45e1052e4213f
Guido Ramírez <guidoramirez1998@hotmail.es>
2024-08-10 02:17:02 -0500

    DEL: debug files ADD: gitignore

--- Debug/Drivers/API/API_log/src/API_log.d
+++
@@ -1,5 +0,0 @@
-Drivers/API/API_log/src/API_log.o: ../Drivers/API/API_log/src/API_log.c \
- /home/guirespi/STM32CubeIDE/GR_1B_FINAL_WORK/1B_PDM_PCDM_FINAL_JOB/Drivers/API/API_log/inc/API_log.h \
- /home/guirespi/STM32CubeIDE/GR_1B_FINAL_WORK/1B_PDM_PCDM_FINAL_JOB/Drivers/API/API_log/arch/common/log_arch_common.h
-/home/guirespi/STM32CubeIDE/GR_1B_FINAL_WORK/1B_PDM_PCDM_FINAL_JOB/Drivers/API/API_log/inc/API_log.h:
-/home/guirespi/STM32CubeIDE/GR_1B_FINAL_WORK/1B_PDM_PCDM_FINAL_JOB/Drivers/API/API_log/arch/common/log_arch_common.h:
--- Debug/Drivers/API/API_spi_flash/src/API_spi_flash.d
+++
@@ -1,2 +0,0 @@
-Drivers/API/API_spi_flash/src/API_spi_flash.o: \
- ../Drivers/API/API_spi_flash/src/API_spi_flash.c
